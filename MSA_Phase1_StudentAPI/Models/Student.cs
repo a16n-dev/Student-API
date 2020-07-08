a90e6abc9f1bc01e2dc6a45dc0754a91be509a7a
Alexander Nicholson <alexn400@gmail.com>
2020-07-09 11:20:51 +1200

    Initial commit

---
+++ MSA_Phase1_StudentAPI/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Student
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int studentId { get; set; }
+    [Required, MaxLength(100)]
+    public string firstName { get; set; }
+    public string middleName { get; set; }
+    [Required]
+    public string lastName { get; set; }
+    public string emailAddress { get; set; }
+    public int phoneNumber { get; set; }
+    [Timestamp]
+    public DateTime timeCreated { get; set; }
+}
